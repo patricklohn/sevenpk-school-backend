75aad8968986705eb481014e49d22537fb929345
Kleber Coelho <klebercoelhogt@gmail.com>
2025-02-14 21:28:04 -0300

    create controller and router

--- src/controllers/categorieController.js
+++ src/controllers/categorieController.js
@@ -24,6 +24,64 @@ async function createCategorie(req, res) {
   }
 }
 
+async function getAll(req, res) {
+  try {
+    const category = await prisma.category.findMany();
+    res.json(category);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function updateCategory(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const { title } = req.body;
+
+    const categoryUpdate = await prisma.category.update({
+      where: { id },
+      data: { title },
+    });
+    res.json({
+      message: "Category updated successfully",
+      category: categoryUpdate,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+async function deleteCategory(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+
+    const categoryDelete = await prisma.category.delete({
+      where: { id },
+    });
+
+    // if (!id) {
+    //   return res.status(404).json({ message: "Category not found" });
+    // }
+
+    // if (isNaN(id)) {
+    //   return res.status(404).json({ message: "Invalid ID" });
+    // }
+
+    
+    res.json({
+      message: "Category deleted successfully",
+      category: categoryDelete,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
 export default {
   createCategorie,
+  getAll,
+  updateCategory,
+  deleteCategory,
 };
--- src/router/categorieRouter.js
+++ src/router/categorieRouter.js
@@ -5,5 +5,8 @@ import categorieController from "../controllers/categorieController.js";
 const categorieRouter = Router();
 
 categorieRouter.post("/categorie", categorieController.createCategorie);
+categorieRouter.get("/categorie", categorieController.getAll);
+categorieRouter.put("/categorie/:id", categorieController.updateCategory);
+categorieRouter.delete("/categorie/:id", categorieController.deleteCategory);
 
 export default categorieRouter;
