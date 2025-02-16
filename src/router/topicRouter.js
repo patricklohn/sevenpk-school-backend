582be0fb23035e535a6f3f8359001b508aad1f90
patricklohn <patricklohn3@gmail.com>
2025-02-15 22:29:44 -0300

    Ajustes no topicRouter e novas migrações

--- src/router/topicRouter.js
+++ src/router/topicRouter.js
@@ -5,5 +5,9 @@ import topicController from "../controllers/topicController.js";
 const topicRouter = Router();
 
 topicRouter.post("/topic", topicController.createTopic);
+topicRouter.get("/topic", topicController.getTopics);
+topicRouter.get("/topic/:id", topicController.getTopic);
+topicRouter.delete("/topic/:id", topicController.deleteTopic);
+topicRouter.put("/topic/:id", topicController.updateTopic);
 
 export default topicRouter;
